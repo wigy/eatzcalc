aadb31ea6ffc45bae89f278d3fcf26675bd0a5ee
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2016-10-12 18:36:59 +0300

    Put some sample data in.

--- app/models/ingredient.js
+++ app/models/ingredient.js
@@ -1,11 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  weight: DS.attr(),
-  energy: DS.attr(),
-  fat: DS.attr(),
-  carbs: DS.attr(),
-  protein: DS.attr(),
-  price: DS.attr()
+    name: DS.attr(),
+    unit: DS.attr(),
+    weight: DS.attr(),
+    energy: DS.attr(),
+    fat: DS.attr(),
+    carbs: DS.attr(),
+    protein: DS.attr(),
+    price: DS.attr(),
 });
--- mirage/config.js
+++ mirage/config.js
@@ -5,6 +5,10 @@ export default function() {
     this.get('/ingredients', function() {
         return {
             data: [
+                {id: 1, type: 'ingredient', name: 'voi', unit: 'g'},
+                {id: 2, type: 'ingredient', name: 'maito', unit: 'dl'},
+                {id: 3, type: 'ingredient', name: 'kananmuna', unit: 'kpl'},
+                {id: 4, type: 'ingredient', name: 'vehnäjauho', unit: 'g'},
             ]
         };
     });

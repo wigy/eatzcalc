5f234f2ce5f46f21ca1c794a32e0b31d51f7a821
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2016-10-12 19:20:07 +0300

    Add some more data and handle models corretly.

--- app/routes/ingredients.js
+++ app/routes/ingredients.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.get('store').findAll('ingredient');
+        let ret = this.get('store').findAll('ingredient');
+
+        ret.then((data) => {
+           let N = data.get('length');
+           for (let i = 0; i < N; i++) {
+               console.log(data.objectAt(i))
+           }
+        });
+        return ret;
     }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -5,10 +5,22 @@ export default function() {
     this.get('/ingredients', function() {
         return {
             data: [
-                {id: 1, type: 'ingredient', name: 'voi', unit: 'g'},
-                {id: 2, type: 'ingredient', name: 'maito', unit: 'dl'},
-                {id: 3, type: 'ingredient', name: 'kananmuna', unit: 'kpl'},
-                {id: 4, type: 'ingredient', name: 'vehnäjauho', unit: 'g'},
+                {id: 1, type: 'ingredient', attributes: {
+                    name: 'voi',
+                    unit: 'g',
+                }},
+                {id: 2, type: 'ingredient', attributes: {
+                    name: 'maito',
+                    unit: 'dl',
+                }},
+                {id: 3, type: 'ingredient', attributes: {
+                    name: 'kananmuna',
+                    unit: 'kpl',
+                }},
+                {id: 4, type: 'ingredient', attributes: {
+                    name: 'vehnäjauho',
+                    unit: 'g',
+                }},
             ]
         };
     });

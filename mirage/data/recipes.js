957d711bf206e9bfeac2c20294833476e5dac1f9
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2016-10-17 19:39:34 +0300

    Fulfill data.

--- mirage/data/recipes.js
+++ mirage/data/recipes.js
@@ -1,12 +1,248 @@
 export default [
-{
-    id: 1, type: 'recipe', attributes: {
-        name: "maitokahvi",
-        description: "Kupillinen kahvia.",
-        parts: [
-            2.5, "kahvi (dl)",
-            0.5, "kevytmaito, Eila (dl)",
-        ]
+    {
+        id: 1, type: 'recipe', attributes: {
+            name: "maitokahvi",
+            description: "Kupillinen kahvia.",
+            parts: [
+                2.5, "kahvi (dl)",
+                0.5, "kevytmaito, Eila (dl)",
+            ]
+        }
+    },
+    {
+        id: 2, type: 'recipe', attributes: {
+            name: "salaattileipä",
+            description: "Yksi leipä vihreällä salaatilla",
+            parts: [
+                1, "ruispala, Pirkka (kpl)",
+                1, "jääsalaatti, Pirkka (lehti)",
+            ]
+        }
+    },
+    {
+        id: 3, type: 'recipe', attributes: {
+            name: "paistettu muna juustogrillimakkaralla",
+            description: "Kaksi kananmunaa ja makkara.",
+            parts: [
+                2, "paistettu kananmuna (kpl)",
+                1, "juustogrillimakkara, Pirkka (kpl)",
+                10, "sinappi, Pirkka (g)",
+            ]
+        }
+    },
+    {
+        id: 4, type: 'recipe', attributes: {
+            name: "lasi maitoa",
+            description: "Punaviinilasillinen maitoa",
+            parts: [
+                2.5, "kevytmaito, Eila (dl)",
+            ]
+        }
+    },
+    {
+        id: 5, type: 'recipe', attributes: {
+            name: "banaani jogurtilla",
+            description: "Kulho johon murskattu banaani ja täytetty jogurtilla reilusti puolilleen.",
+            parts: [
+                245, "maustamaton AB-jogurtti, Pirkka (g)",
+                1, "banaani (kpl)",
+            ]
+        }
+    },
+    {
+        id: 6, type: 'recipe', attributes: {
+            name: "jogurtti",
+            description: "Kulho jogurttia reilusti puolilleen.",
+            parts: [
+                245, "maustamaton AB-jogurtti, Pirkka (g)",
+            ]
+        }
+    },
+    {
+        id: 7, type: 'recipe', attributes: {
+            name: "omena",
+            description: "Yksi keskikokoinen omena.",
+            parts: [
+                1, "omena (kpl)",
+            ]
+        }
+    },
+    {
+        id: 8, type: 'recipe', attributes: {
+            name: "paistettu muna",
+            description: "Yksi leipä vihreällä salaatilla",
+            parts: [
+                1, "paistettu kananmuna (kpl)",
+            ]
+        }
+    },
+    {
+        id: 9, type: 'recipe', attributes: {
+            name: "perussalaatti",
+            description: "Puoli kulhollista salaattia.",
+            parts: [
+                65, "jääsalaatti, Pirkka (g)",
+                1, "tomaatti (kpl)",
+                83, "kurkku (g)",
+                10, "auringonkukkaöljy (g)",
+            ]
+        }
+    },
+    {
+        id: 10, type: 'recipe', attributes: {
+            name: "raejuusto",
+            description: "Purkkia raejuustoa.",
+            parts: [
+                200, "raejuusto, Valio (g)",
+            ]
+        }
+    },
+    {
+        id: 11, type: 'recipe', attributes: {
+            name: "palautusjuoma",
+            description: "Yksi tölkki.",
+            parts: [
+                1, "palautusjuoma, Sportyfeel (kpl)",
+            ]
+        }
+    },
+    {
+        id: 12, type: 'recipe', attributes: {
+            name: "paistetut broilerin suikaleet",
+            description: "Yksi neljänneskilon (mitattuna 240g) paketti broileria.",
+            parts: [
+                240, "broilerin fileesuikale, Kariniemi (g)",
+                20, "öljyseos, Flora (g)",
+            ]
+        }
+    },
+    {
+        id: 13, type: 'recipe', attributes: {
+            name: "katkarapunuudelit",
+            description: "Yksi pussi.",
+            parts: [
+                1, "nuudeli, katkarapu, Mama (pussi)",
+                1.2, "vesi (dl)",
+            ]
+        }
+    },
+    {
+        id: 14, type: 'recipe', attributes: {
+            name: "grillimakkara",
+            description: "Yksi makkara sinapilla.",
+            parts: [
+                1, "grillimakkara, Snellman Iivari (kpl)",
+                10, "sinappi, Pirkka (g)",
+            ]
+        }
+    },
+    {
+        id: 15, type: 'recipe', attributes: {
+            name: "kiivi jogurtilla",
+            description: "Kulho johon silputtu kiivi ja täytetty jogurtilla reilusti puolilleen.",
+            parts: [
+                245, "maustamaton AB-jogurtti, Pirkka (g)",
+                1, "kiivi (kpl)",
+            ]
+        }
+    },
+    {
+        id: 16, type: 'recipe', attributes: {
+            name: "munat ja pekoni",
+            description: "Kaksi kananmunaa ja kolmannes pekonipaketista.",
+            parts: [
+                2, "paistettu kananmuna (kpl)",
+                0.3, "pekoni, Pirkka (pkt)",
+            ]
+        }
+    },
+    {
+        id: 17, type: 'recipe', attributes: {
+            name: "juustokauraleipä",
+            description: "Yksi leipä ja noin 2.5 siivua juustoa.",
+            parts: [
+                1, "kauraleipä, Vaasan kaurasydän (kpl)",
+                5, "margariini, Oivariini (g)",
+                30, "juusto, Valio Edam (g)",
+            ]
+        }
+    },
+    {
+        id: 18, type: 'recipe', attributes: {
+            name: "rapea kalaleike",
+            description: "Yksi kappale.",
+            parts: [
+                1, "rapea kalaleike, Pirkka (kpl)",
+            ]
+        }
+    },
+    {
+        id: 19, type: 'recipe', attributes: {
+            name: "lasi jaffaa",
+            description: "Punaviinilasillinen jaffaa.",
+            parts: [
+                1, "jaffa light, Hartwall (lasi)",
+            ]
+        }
+    },
+    {
+        id: 20, type: 'recipe', attributes: {
+            name: "mansikkarahka",
+            description: "Punaviinilasillinen jaffaa.",
+            parts: [
+                1, "mansikkarahka, Valio Pro feel (kpl)",
+            ]
+        }
+    },
+    {
+        id: 21, type: 'recipe', attributes: {
+            name: "juustometwurstikauraleipä",
+            description: "Yksi leipä, noin 2.5 siivua juustoa ja neljä metwurstia.",
+            parts: [
+                1, "kauraleipä, Vaasan kaurasydän (kpl)",
+                5, "margariini, Oivariini (g)",
+                4, "kevytmeetwursti, Pirkka (kpl)",
+                30, "juusto, Valio Edam (g)",
+            ]
+        }
+    },
+    {
+        id: 22, type: 'recipe', attributes: {
+            name: "viherpippurijauhelihakastike",
+            description: "Neljä annosta. Tehdään 3dl:aan maitoa.",
+            parts: [
+                400, "jauheliha, sika-nauta (g)",
+                1, "sipuli, iso (kpl)",
+                20, "öljyseos, Flora (g)",
+                1, "viherpippurikastike, Knorr (kpl)",
+            ]
+        }
+    },
+    {
+        id: 23, type: 'recipe', attributes: {
+            name: "keitetyt perunat",
+            description: "Laskettu per yksi keskikokoinen peruna.",
+            parts: [
+                56, "peruna (g)",
+            ]
+        }
+    },
+    {
+        id: 24, type: 'recipe', attributes: {
+            name: "suolakurkku",
+            description: "Yksi suolakurkku.",
+            parts: [
+                1, "suolakurkku (kpl)",
+            ]
+        }
+    },
+    {
+        id: 25, type: 'recipe', attributes: {
+            name: "olut",
+            description: "Lager olut, 0.5l.",
+            parts: [
+                1, "lagerolut, A Le Cog (0.5l)",
+            ]
+        }
     }
-},
-]
+];
